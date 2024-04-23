2ea8790553b213b1b08cff0b71179e603ba1e25e
DummyCoding <modestopellerin@gmail.com>
2024-04-23 20:24:58 +0800

    Rearrange mode of procurement drop-down alphabetically

--- app/projects/forms.py
+++ app/projects/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, TextAreaField, DecimalField, SelectField, DateF
 from wtforms.validators import DataRequired
 
 class ProjectForm(FlaskForm):
-    mode_choices = [('Competitive Bidding', 'Competitive Bidding'), ('Direct Contracting', 'Direct Contracting'), ('Shopping', 'Shopping'), ('Small Value Procurement', 'Small Value Procurement'), ('Agency-to-Agency', 'Agency-to-Agency'), ('Negotiated Procurement', 'Negotiated Procurement')]
+    mode_choices = [('Competitive Bidding', 'Competitive Bidding'), ('Agency-to-Agency', 'Agency-to-Agency'), ('Direct Contracting', 'Direct Contracting'), ('Negotiated Procurement', 'Negotiated Procurement'), ('Shopping', 'Shopping'), ('Small Value Procurement', 'Small Value Procurement')]
     
     title = StringField('Title', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
