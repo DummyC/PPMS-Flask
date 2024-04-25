90faeb85ab7fb14b609fc2b0b9bc19047034e9a6
DummyCoding <modestopellerin@gmail.com>
2024-04-25 22:45:50 +0800

    Replace mail sender

--- app/users/utils.py
+++ app/users/utils.py
@@ -4,7 +4,7 @@ from app import mail
 
 def send_reset_email(user):
     token = user.get_reset_token()
-    msg = Message('Password Reset Request', sender='noreply@demo.com', recipients=[user.email])
+    msg = Message('Password Reset Request', sender='no-reply@bisu.edu.ph', recipients=[user.email])
     msg.body = f'''To reset your password, vist the following link:
 {url_for('users.reset_token', token=token, _external=True)}
 
