1670e0deab305039813ca82aa19f62241a1013b0
DummyCoding <modestopellerin@gmail.com>
2024-05-03 15:47:34 +0800

    Database URI as environment variable

--- app/config.py
+++ app/config.py
@@ -6,7 +6,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/ppms?charset=utf8mb4'
     # use sqlite for development 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
